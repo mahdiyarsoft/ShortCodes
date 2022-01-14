0daa88931e5a372dba6c6be5a793373278dbad31
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 12:47:29 +0330

    Create script.js

---
+++ js/ajax/script.js
@@ -0,0 +1,11 @@
+function loadDoc() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("main").innerHTML =
+                        this.responseText;
+                }
+            };
+            xhttp.open("GET", "main.html", true);
+            xhttp.send();
+        }
